9234653609fe2b623635978dae422d40441aadbf
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-04-13 17:08:19 +0300

    Создать схемы mongoose, user и movie

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { isEmail, isURL } = require('validator');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email) => isEmail(email),
+        message: 'Передан невалидный email адрес.',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+module.exports = mongoose.model('user', userSchema);
