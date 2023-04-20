0ca6828a01efbd5677a4db89c1f4063de010a717
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-04-21 01:38:08 +0300

    Подготовка к деплою

--- app.js
+++ app.js
@@ -10,30 +10,16 @@ const limiter = require('./middlewares/limiter');
 const { PORT, DB_ADDRESS } = require('./utils/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes');
-// const auth = require('./middlewares/auth');
-// const { validateSignUp, validateSignIn } = require('./middlewares/validate');
-// const { createUser, login, signOut } = require('./controllers/users');
 const errorHandler = require('./middlewares/errorHandler');
-// const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 app.use(helmet());
 app.use(limiter);
 app.use(cors(options));
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
-
 app.use(router);
-// app.post('/signup', validateSignUp, createUser);
-// app.post('/signin', validateSignIn, login);
-// app.post('/signout', signOut);
-
-// app.use(auth);
-// app.use(router);
-// app.use('*', (req, res, next) => next(new NotFoundError('Страница не существует.')));
-
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ const NotFoundError = require('../errors/NotFoundError');
 
 router.post('/signup', validateSignUp, createUser);
 router.post('/signin', validateSignIn, login);
-
 router.use(auth);
 router.use('/users', users);
 router.use('/movies', movies);
