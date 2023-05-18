7cfa18cea414b39a1052538b871e6ca4b1dd1116
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-05-18 17:56:39 +0300

    Перестановка

--- middlewares/limiter.js
+++
@@ -1,6 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-module.exports = rateLimit({
-  windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 100, // можно совершить максимум 100 запросов с одного IP
-});
--- utils/constants.js
+++
@@ -1,9 +0,0 @@
-const { PORT = 3000 } = process.env;
-const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
-const { NODE_ENV = 'dev' } = process.env;
-const PROD = 'production';
-const SECRET_KEY = 'some-secret-key';
-
-module.exports = {
-  PORT, DB_ADDRESS, NODE_ENV, PROD, SECRET_KEY,
-};
